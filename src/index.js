cac4588a56c07a2fbf384f630b56614795aeee8d
PoojaPoluru <pooja.poluru95@gmail.com>
2018-07-07 12:31:36 -0700

    changes made to index.js and index.html files

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+
+
+const App=()=>{
+    return(
+        <div>Hello!</div>
+    );
+}
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
